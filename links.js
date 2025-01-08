6508996a656f6c8e6114d465a49e2596c21d8d21
Anika Viswanathan <anikaviswanathan@gmail.com>
2025-01-07 23:19:47 -0500

    Links JS

--- links.js
+++ links.js
@@ -1,1 +1,13 @@
+const pinterestIcon = document.getElementById("pinterest");
+const spotifyIcon = document.getElementById("spotify");
 
+pinterestIcon.addEventListener("click", pinterestLink());
+spotifyIcon.addEventListener("click", spotifyLink());
+
+function pinterestLink() {
+	window.open("https://www.pinterest.com/anikaviswanathan/", "_blank").focus;
+}
+
+function spotifyLink() {
+	window.open("https://open.spotify.com/user/8gni29yt6txpbpd5mcxknr7v2?si=3565896090cb49f9", "_blank").focus;
+}
